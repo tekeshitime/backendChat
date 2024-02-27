5cdb37a08b82947d83afdb0ef581975b4faa66e3
tekeshitime <kyaputenn13@gmail.com>
2024-02-27 14:41:34 +0900

    change origin cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: ["https://front-chat-bdcq3nqlk-tekeshitime.vercel.app/"],
+    origin: ["https://front-chat-zeta.vercel.app/"],
   },
 });
 
